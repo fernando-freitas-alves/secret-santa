409466b035425d6dcf89476f4d67267625a5f6a0
Fernando Freitas Alves <ffreitas@mimic.com.br>
2022-04-15 04:08:34 -0300

    :new: (graph) add check for people that may not give or receive any gift

--- src/utils/collection.py
+++ src/utils/collection.py
@@ -1,11 +1,24 @@
 __all__ = [
     "create_pairs",
+    "get_non_repeated_items",
 ]
 
 from itertools import combinations
-from typing import Any, FrozenSet, Iterable
+from typing import Any, FrozenSet, Iterable, Set
 
 
 def create_pairs(iterable: Iterable[Any]) -> FrozenSet[FrozenSet[Any]]:
     pairs = combinations(iterable, r=2)
     return frozenset((frozenset(pair) for pair in pairs))
+
+
+def get_non_repeated_items(iterable: Iterable[Any]) -> FrozenSet[Any]:
+    repeated: Set[Any] = set()
+    non_repeated: Set[Any] = set()
+    for item in iterable:
+        if item in non_repeated:
+            repeated.add(item)
+            non_repeated.remove(item)
+        else:
+            non_repeated.add(item)
+    return frozenset(non_repeated)
